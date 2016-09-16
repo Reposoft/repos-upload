8de1e95700b9119f6ac16397db85b5c5857fb06b
Anton Lindgren <lindgren.anton@gmail.com>
2016-09-16 08:41:15 +0200

    Start on build-contract

---
+++ build-contracts/client.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+chai.should();
+
+const reposUpload = require('repos-upload');
+const uuid = require('uuid');
+
+describe('repos-upload', function () {
+
+  it('should support writing several files to the same (missing) folder', () => {
+
+    const reposHost = new reposUpload.ReposUpload({
+      hostname: 'http://rweb',
+      dataRepository: '/svn/lean-data'
+    });
+
+    const ps = [];
+    const path = `/svn/lean-data/${uuid.v4()}/${uuid.v4()}`;
+    for (let i = 0; i < 10; i++) {
+      ps.push(new Promise((resolve, reject) => {
+        const id = uuid.v4();
+        reposHost.createFile(`${path}/${id}.txt`, id, err => {
+          if (err) reject(err);
+          else resolve(id);
+        });
+      }));
+    }
+
+    return Promise.all(ps);
+  });
+});
